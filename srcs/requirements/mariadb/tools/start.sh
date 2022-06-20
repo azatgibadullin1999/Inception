92cfba4c205cc64385101011180452ed02259e7f
lil chicha <azat251061@gmail.com>
2022-06-20 15:45:27 +0300

    added scritp for create sql database

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 service mysql start
+mysql -u root --password="" <<EOF
+CREATE DATABASE wordpressdb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpressdb.* TO 'admin'@'%' IDENTIFIED BY 'admin';
+FLUSH PRIVILEGES;
+EOF
+
+bash
