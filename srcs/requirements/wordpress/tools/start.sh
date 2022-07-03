f281a55059263a3132221c5def46baf9285082b4
larlena <larlena@larlena.larlena.42.fr>
2022-07-03 17:44:57 -0400

    added start php-fpm no daemon

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ -e /var/www/html/wordpress/index.php ]; then
+# if [ -e /var/www/html/wordpress/index.php ]; then
 	wp core download --allow-root
 	wp config create --dbname=${DB_NAME} --dbuser=${DB_ADMIN} --dbpass=${DB_ADMIN_PASS} --dbhost=${DB_HOST} --allow-root
 	wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --allow-root
-fi
-service php7.3-fpm start
-service php7.3-fpm status
-bash
+# fi
+# service php7.3-fpm start
+# service php7.3-fpm status
+# bash
+/usr/sbin/php-fpm7.3 --nodaemonize
