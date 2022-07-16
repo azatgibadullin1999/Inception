f8b85a2cc5ad4fed925bf1be06f2f5fff629a980
azatgibadullin1999 <azat251061@gmail.com>
2022-07-16 11:41:16 -0400

    move creating to dockerfile, change bind-address

--- srcs/requirements/mariadb/conf/create_db.sql
+++ srcs/requirements/mariadb/conf/create_db.sql
@@ -1,9 +1,3 @@
-#!/bin/bash
-service mysql start
-mysql -u root --password="" <<EOF
 CREATE DATABASE IF NOT EXISTS wordpressdb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL ON wordpressdb.* TO 'larlena'@'%' IDENTIFIED BY 'da_da_ya';
 FLUSH PRIVILEGES;
-EOF
-
-/usr/bin/mysqld_safe
